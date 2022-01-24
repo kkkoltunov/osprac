d574f1ec1aac455691930aa0963001d5aeabe759
kkkoltunov <kkkoltunov@edu.hse.ru>
2022-01-24 17:15:09 +0300

    fix mistake

--- Sem01-02/task1.c
+++ Sem01-02/task1.c
@@ -2,7 +2,7 @@
 #include <unistd.h>
 
 int main(int argc, char **argv) {
-  printf("Hello, your id = %d\n" + getuid());
-  printf("group id = %d\n" + getgid());
+  printf("Hello, your id = %d\n", getuid());
+  printf("group id = %d\n", getgid());
   return 0;
 }
