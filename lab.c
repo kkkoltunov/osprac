4b62f412f468f7113097597f5d2c3bacd478da07
Kirill Koltunov <kkkoltunov@edu.hse.ru>
2022-06-15 23:34:36 +0300

    deltete lab.c

--- lab.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-#include <sys/types.h>
-
-int main(int argc, char **argv) {
-    printf("Hello, your id = %d\n" + getuid());
-    printf("group id = %d\n" + getgid());
-return 0;
-}
